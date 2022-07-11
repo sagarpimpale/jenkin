b30d8c4dc91ddf9ac8097b66624d77b6f3f452bb
𝕾𝖆𝖌𝖆𝖗 𝕻𝖎𝖒𝖕𝖆𝖑𝖊 <sagarpimple16@gmail.com>
2022-07-11 20:29:28 +0530

    added

---
+++ poem.py
@@ -0,0 +1,7 @@
+print('''twinkle,twinkle,little star,
+              how I wonder what you are!
+                     Up above the world so high,
+                     like a diamond in the sky.
+      Twinkle,Twinkle, little star,
+              how i wonder what you are''')
+               
